8895818a40212353fc310494912df39efc3caa57
2018331043 <mahnur43@student.sust.edu>
2021-03-20 03:31:39 +0600

    Continuous Sorting Add

--- Sorting A Folder/sorting_folder.py
+++ Sorting A Folder/sorting_folder.py
@@ -1,22 +1,27 @@
 import os
 import shutil
+import time
 
 path="C:\\Users\\ASUS\\Downloads"
 Extenstions=[[".JPEG",".jpg",".jpeg",".png",".JPG"],[".mkv",".mp4",".mpeg"],[".txt",".doc",".docx",".pptx",".pdf"],[".exe"],[".torrent"],[".zip"]]
 Formats=["Images","Videos","Documents","Executables","Torrents","Zip Files","Others"]
-filenames=os.listdir(path);
+
 for i in Formats:
 	if not os.path.exists(path+"\\"+i):
 		os.makedirs(path+"\\"+i)
 		print("Entered in making folder")
-x=0
-for i in Extenstions:
-	for j in i:
-		for name in filenames:
-			if name.endswith(j) and not os.path.exists(path+"\\"+Formats[x]+"\\"+name):
-				print("Entered")
-				shutil.move(path+"\\"+name,path+"\\"+Formats[x]+"\\"+name) 
-	x=x+1
+
+while True:
+	time.sleep(2)
+	x=0
+	filenames=os.listdir(path);
+	for i in Extenstions:
+		for j in i:
+			for name in filenames:
+				if name.endswith(j) and not os.path.exists(path+"\\"+Formats[x]+"\\"+name):
+					print("Entered")
+					shutil.move(path+"\\"+name,path+"\\"+Formats[x]+"\\"+name) 
+		x=x+1
 
 
 """if not os.path.exists(path+"\Images"):
