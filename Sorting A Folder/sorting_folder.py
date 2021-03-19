8faf8c01211f464aca3f7470f35b5570d93559a3
2018331043 <mahnur43@student.sust.edu>
2021-03-20 02:32:44 +0600

    Basic Sorting folder functionality done

--- Sorting A Folder/sorting_folder.py
+++ Sorting A Folder/sorting_folder.py
@@ -1,9 +1,25 @@
 import os
 import shutil
 
-path="E:\Python Automation Projects\Python_Automation_Projects\Sorting A Folder\TestFolder"
+path="C:\\Users\\ASUS\\Downloads"
+Extenstions=[[".JPEG",".jpg",".jpeg",".png",".JPG"],[".mkv",".mp4",".mpeg"],[".txt",".doc",".docx",".pptx",".pdf"],[".exe"],[".torrent"],[".zip"]]
+Formats=["Images","Videos","Documents","Executables","Torrents","Zip Files","Others"]
 filenames=os.listdir(path);
-if not os.path.exists(path+"\Images"):
+for i in Formats:
+	if not os.path.exists(path+"\\"+i):
+		os.makedirs(path+"\\"+i)
+		print("Entered in making folder")
+x=0
+for i in Extenstions:
+	for j in i:
+		for name in filenames:
+			if name.endswith(j) and not os.path.exists(path+"\\"+Formats[x]+"\\"+name):
+				print("Entered")
+				shutil.move(path+"\\"+name,path+"\\"+Formats[x]+"\\"+name) 
+	x=x+1
+
+
+"""if not os.path.exists(path+"\Images"):
 	os.makedirs(path+"\Images")
 	print("Entered")
 for name in filenames:
@@ -11,4 +27,4 @@ for name in filenames:
 	temp= temp and not os.path.exists(path+"\Images"+"\\"+name)
 	if temp:
 		print("Entered")
-		shutil.move(path+"\\"+name,path+"\Images"+"\\"+name) 
+		shutil.move(path+"\\"+name,path+"\Images"+"\\"+name) """
